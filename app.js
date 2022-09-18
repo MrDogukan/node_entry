8931c2edf3d9083cafb193b0479bc53b9ba30e5b
aliseslikaya <aliseslikaya@gmail.com>
2022-09-18 22:01:20 +0300

    mongoose added

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb
 
-app.use(morgan("tiny"));
+const dbURI =
+  "mongodb+srv://aliseslikaya:19676161@nodeses.plurxry.mongodb.net/node-ses?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set("view engine", "ejs");
+
+// middleware
+app.use(express.static("public"));
+app.use(morgan("tiny"));
+
 // app.set('views', 'myviews');
 
 app.get("/", (req, res) => {
